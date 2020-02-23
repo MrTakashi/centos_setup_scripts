317cd91f9e27eeab21a5fb9c0f1f6480dbb012d2
MrTakashi <46873312+MrTakashi@users.noreply.github.com>
2020-02-24 00:44:27 +0300

    Update yum.sh

--- yum.sh
+++ yum.sh
@@ -6,28 +6,28 @@ echo "$(tput setaf 3)$ yum check-update && yum -y update$(tput sgr 0)"
 yum check-update && yum -y update
 
 # Install and configure yum-cron for automatic updates
-echo ">>> Install and configure yum-cron for automatic updates"
-echo "$ yum install -y yum-cron"
+echo "$(tput setaf 2)[Install and configure yum-cron for automatic updates]$(tput sgr 0)"
+echo "$(tput setaf 3)$ yum install -y yum-cron$(tput sgr 0)"
 yum install -y yum-cron
 	
 # Say yum-cron to setup security updates automaticly
 #	vi /etc/yum/yum-cron.conf
 #		update_cmd = security		
 #		apply_updates = yes			
-echo ">>> Say yum-cron to setup security updates automaticly"
-echo "$ sed -i -r 's/^update_cmd = default.*/update_cmd = security/' /etc/yum/yum-cron.conf"
-echo "$ sed -i -r 's/^apply_updates = no.*/apply_updates = yes/' /etc/yum/yum-cron.conf"
+echo "$(tput setaf 2)[Say yum-cron to setup security updates automaticly]$(tput sgr 0)"
+echo "$(tput setaf 3)$ sed -i -r 's/^update_cmd = default.*/update_cmd = security/' /etc/yum/yum-cron.conf$(tput sgr 0)"
+echo "$(tput setaf 3)$ sed -i -r 's/^apply_updates = no.*/apply_updates = yes/' /etc/yum/yum-cron.conf$(tput sgr 0)"
 sed -i -r 's/^update_cmd = default.*/update_cmd = security/' /etc/yum/yum-cron.conf
 sed -i -r 's/^apply_updates = no.*/apply_updates = yes/' /etc/yum/yum-cron.conf
 
 #check: grep -P "(update_cmd|apply_updates)" /etc/yum/yum-cron.conf
 
 # Setup new autorun service
-echo ">>> Setup new autorun service"
-echo "$ systemctl enable yum-cron && systemctl start yum-cron && systemctl status yum-cron"
+echo "$(tput setaf 2)[Setup new autorun service]$(tput sgr 0)"
+echo "$(tput setaf 3)$ systemctl enable yum-cron && systemctl start yum-cron && systemctl status yum-cron$(tput sgr 0)"
 systemctl enable yum-cron && systemctl start yum-cron && systemctl status yum-cron
 
 #check
-echo ">>> Check"
-echo "$ tail /var/log/yum.log"
+echo "$(tput setaf 2)[Check]$(tput sgr 0)"
+echo "$(tput setaf 3)$ tail /var/log/yum.log$(tput sgr 0)"
 tail /var/log/yum.log
