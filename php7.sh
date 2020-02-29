298d6526db8ef351e2488256b20cc27846b3ce71
MrTakashi <46873312+MrTakashi@users.noreply.github.com>
2020-02-29 23:58:56 +0300

    Create php7.sh

---
+++ php7.sh
@@ -0,0 +1,8 @@
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum -y install yum-utils
+yum-config-manager --enable remi-php74
+yum -y update
+yum -y install php  php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json
+php -v
+php --modules
