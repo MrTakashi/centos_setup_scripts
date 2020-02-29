9d7351d6a1c0e8bf4ff6956134b1d50c19355164
MrTakashi <46873312+MrTakashi@users.noreply.github.com>
2020-03-01 00:40:24 +0300

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,3 +1,7 @@
+echo "$(tput setaf 2)[Install Apache]$(tput sgr 0)"
+echo "$(tput setaf 3)$ yum -y install httpd$(tput sgr 0)"
 yum -y install httpd
 
+echo "$(tput setaf 2)[Setup autorun$(tput sgr 0)"
+echo "$(tput setaf 3)$ systemctl enable httpd.service && systemctl start httpd.service && systemctl status httpd.service$(tput sgr 0)"
 systemctl enable httpd.service && systemctl start httpd.service && systemctl status httpd.service
