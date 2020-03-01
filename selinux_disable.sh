86b3f40710ef479208771d1695a30b3717a97e48
MrTakashi <46873312+MrTakashi@users.noreply.github.com>
2020-03-01 18:22:20 +0300

    Update selinux_disable.sh

--- selinux_disable.sh
+++ selinux_disable.sh
@@ -1,22 +1,19 @@
 #check
 echo "$(tput setaf 2)[Check SE Linux status]$(tput sgr 0)"
 echo "$(tput setaf 3)$ sestatusg$(tput sgr 0)"
-
 sestatus
+
 #getenforce
 #cat /etc/selinux/config
 
-#disable
+#disable selinux
 #vi /etc/selinux/config
 #SELINUX=disabled
 echo "$(tput setaf 2)[Disable SE Linux]$(tput sgr 0)"
 echo "$(tput setaf 3)$ sed -i -r 's/^SELINUX=enforcing.*/SELINUX=disabled/' /etc/selinux/config$(tput sgr 0)"
-
 sed -i -r 's/^SELINUX=enforcing.*/SELINUX=disabled/' /etc/selinux/config
 
-echo "$(tput setaf 2)[Please reboot...]$(tput sgr 0)"
-
+echo "$(tput setaf 2)[You need reboot to apply changes...]$(tput sgr 0)"
 #reboot & check
 #reboot
-
 #sestatus
