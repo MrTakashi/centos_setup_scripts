0850fb1d31e403964dbfb9378795d19e52bfbef5
MrTakashi <46873312+MrTakashi@users.noreply.github.com>
2021-03-28 22:09:34 +0300

    Update php7.sh

--- php7.sh
+++ php7.sh
@@ -3,6 +3,6 @@ yum -y install https://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum -y install yum-utils
 yum-config-manager --enable remi-php74
 yum -y update
-yum -y install php  php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json
+yum -y install php php-cli php-fpm php-mysqlnd php-zip php-devel php-gd php-mcrypt php-mbstring php-curl php-xml php-pear php-bcmath php-json
 php -v
 php --modules
